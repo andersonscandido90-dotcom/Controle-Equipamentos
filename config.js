933af4533f90c93cff493c31a2cecd2311f43727
andersonscandido90-dotcom <andersonscandido90@gmail.com>
2025-12-09 21:37:34 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+javascript
+// js/config.js
+const CONFIG = {
+    estados: [
+        { classe: "verde", texto: "Na linha", cor: "#4CAF50" },
+        { classe: "amarela", texto: "com Restrição", cor: "#ffcc00" },
+        { classe: "azul", texto: "de Serviço", cor: "#2196F3" },
+        { classe: "", texto: "Disponível", cor: "#f0f0f0" },
+        { classe: "vermelho", texto: "Indisponível", cor: "#f44336" }
+    ],
+    equipamentos: [
+        "MCP BB", "MCP BE", "MCA 1", "MCA 2", "MCA 3", "MCA 4", "Gerador de Emerg", 
+        "URA 1", "URA 2", "URA 3", "URA 4", "URA 5", "URA 6",
+        "GOR 1", "GOR 2", "GOR 3", "GOR 4", "DEMIN",
+        "Maquina do Leme BE 1", "Maquina do Leme BE 2", 
+        "Maquina do Leme BB 1", "Maquina do Leme BB 2",
+        "HPSW 1", "HPSW 2", "HPSW 3", "HPSW 4", "HPSW 5",
+        "LPSW 1", "LPSW 2", "LPSW 3", "LPSW 4",
+        "Bomba de Serviço 1", "Bomba de Serviço 2", "Bomba de Serviço 3",
+        "MotoBomba 1", "MotoBomba 2", "MotoBomba 3", "MotoBomba 4",
+        "CAP 1", "CAP 2", "CAP 3", "CMP 1", "CMP 2", "CMP de Emergência",
+        "CBP 1", "CBP 2", "CBP 3", "CBP 4",
+        "Planta Frigorífica 1", "Planta Frigorífica 2",
+        "Estabilizador BB", "Estabilizador BE", 
+        "BAG 1", "BAG 2", "BAG 3", "BAG 4",
+        "BOILER 1", "BOILER 2", "BOILER 3", "BOILER 4",
+        "Bomba de Água Quente 1", "Bomba de Água Quente 2",
+        "Separador de Óleo/Água",
+        "Purificador Óleo Comb 1", "Purificador Óleo Comb 2",
+        "Purificador Óleo Lub 1", "Purificador Óleo Lub 2",
+        "Purificador Redutora 1", "Purificador Redutora 2",
+        "Purificador Óleo Lub GER Diesel",
+        "Elevador de Aeronaves AV", "Elevador de Aeronaves AR",
+        "Guindaste", "Container 1", "Container 2", "Container 3",
+        "Proteção Catódica AV", "Proteção Catódica AR"
+    ],
+    navio: {
+        nome: "NAM ATLÂNTICO - A140",
+        brasao: "assets/images/logo.png"
+    }
+};
